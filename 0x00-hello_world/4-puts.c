449fda89dd26eb46b1c8ed76c43714cdd5268573
Goddaybc <goddayoghenerobiz@gmail.com>
2022-06-30 14:11:10 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-
+#include<stdio.h>
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"*
- *    * Return: Always 0 (Success)
- *     */
-
+ * main -entry point
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-}}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

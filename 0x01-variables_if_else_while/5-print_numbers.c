7e900421a0001343551006483ac037668d8c0621
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-01 16:44:57 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,6 +9,7 @@ int main(void)
 {
 int num;
 for (num = 0; num < 10; num++)
-printf("%d", num);
+putchar((num % 10) + '0');
+putchar('\n');
 return (0);
 }

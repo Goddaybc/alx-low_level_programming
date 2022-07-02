d77b65dccc3cafd2753e1c806d7c1f7e7f097df8
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-01 17:34:25 -0700

    numbi

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}

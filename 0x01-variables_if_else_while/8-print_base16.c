723b518a6c8af48769f67147ce7acfb72708803c
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-01 18:15:56 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch;
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}

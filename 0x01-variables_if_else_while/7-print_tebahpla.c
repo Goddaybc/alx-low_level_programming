4a574d07b1255f14b073d2aa188178f31ed4356e
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-01 17:41:20 -0700

    teb

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}

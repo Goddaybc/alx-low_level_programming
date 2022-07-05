c5a0c45146b72b65227aa8859c1f813b789b5c87
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-05 14:31:05 -0700

    loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (c = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return (0);
+}

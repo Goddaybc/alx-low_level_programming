1e7440e8afa1a48a4bead0f3a5949473f35d1e6f
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-12 09:32:09 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - print every other characters
+ * @str: argument
+ * Description: return the required result
+ * Return: return (void)
+ */
+void puts2(char *str)
+{
+char *ptr = str;
+int len = 0;
+int j;
+
+while (*ptr != '\0')
+{
+len++;
+ptr++;
+}
+for (j = 0; j <= (len - 1); j++)
+{
+if (j % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}

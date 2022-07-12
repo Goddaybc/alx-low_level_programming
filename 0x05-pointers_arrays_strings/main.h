390163d0e19f6c9d920ac8ff8752a4feb7563da1
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-12 09:38:59 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#ifndef MAIN.H
+#define MAIN.H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,3 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+
+#endif

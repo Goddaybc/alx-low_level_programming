9b59fe7316901d7abd4516ef7ce077a70e97ba71
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-14 18:17:05 -0700

    12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,41 +2,44 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer 10 bytes at a time, starting with
- * the byte position,then showing the hex content,
- * then displaying printable characters
- * @b: the buffer to be printed
- * @size: The number of bytes to be printed from the buffer
+ * print_buffer - print a buffer
+ * @b: the buffer to print
+ * @size: the number of bytes to prints
+ *
+ * Description: this prints the contents of ofsize bytes of the
+ * buffer pointed to by b. 10 bytes are printed per line, with each line
+ * prefixed with the position of the first byte, starting at 0. Each line
+ * shows the heaxadecimalcontent of the buffers 2 bytes at a time
+ * each pair separated by a space,followed by the content of the buffer
+ * with non-printable characters show as a '.'.
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-int byte, index;
-for (byte = 0; byte < size; byte += 10)
+int b_pos;
+int l_pos;
+for (b_pos = 0; b_pos < size; b_pos += 10)
 {
-printf("%08x: ", byte);
-for (index = 0; index < 10; index++)
+printf("%08x: ", b_pos);
+for (l_pos = 0; l_pos < 10; ++l_pos)
 {
-if ((index + byte) >= size)
-printf(" ");
+if (b_pos + l_pos < size)
+printf("%02x", b[b_pos + l_pos]);
 else
-printf("%02x", *(b + index + byte));
-if ((index % 2) != 0 && index != 0)
-printf(" ");
+printf("  ");
+if (l_pos % 2)
+putchar(' ');
 }
-for (index = 0; index < 10; index++)
+for (l_pos = 0; l_pos < 10 && b_pos + l_pos < size; ++l_pos)
 {
-if ((index + byte) >= size)
-break;
-else if
-(*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-printf("%c", *(b + index + byte));
+if (b[b_pos + l_pos] < 32 || b[b_pos + l_pos] > 126)
+putchar('.');
 else
-printf(".");
+putchar(b[b_pos + l_pos]);
 }
-if (byte >= size)
-continue;
-printf("\n");
+if (b_pos + l_pos < size)
+putchar('\n');
 }
-if (size <= 0)
-printf("\n");
+putchar('\n');
 }

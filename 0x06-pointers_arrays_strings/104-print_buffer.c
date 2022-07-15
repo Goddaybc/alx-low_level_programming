b8bd8956f7f9e0bf03937dde26a0fb92fe36bfe6
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-14 17:24:12 -0700

    12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,42 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - Function that prints content of input number
- * @b: character args
- * @size: int args
+ * print_buffer - prints a buffer 10 bytes at a time, starting with
+ * the byte position,then showing the hex content,
+ * then displaying printable characters
+ * @b: the buffer to be printed
+ * @size: The number of bytes to be printed from the buffer
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j;
-
-	if (size < 0)
-	{
-		printf('\n');
-		return;
-	}
-
-	while (i < size)
-	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
-		{
-			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
-			else
-			{
-				while (++j <= i + 10)
-					printf(" ");
-				printf(" ");
-			}
-		}
-		for (j = i; j < i + 9 && j < size; j++)
-		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
-		}
-		printf('\n');
-		i += 10;
-	}
+int byte, index;
+for (byte = 0; byte < size; byte += 10)
+{
+printf("%08x: ", byte);
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+printf(" ");
+else
+printf("%02x", *(b + index + byte));
+if ((index % 2) != 0 && index != 0)
+printf(" ");
+}
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+break;
+else if
+(*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+printf("%c", *(b + index + byte));
+else
+printf(".");
+}
+if (byte >= size)
+continue;
+printf("\n");
+}
+if (size <= 0)
+printf("\n");
 }

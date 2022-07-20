389d7dccc1d662054de4e8b6a1a84e880d56f55c
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-20 11:12:57 -0700

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,20 +1,42 @@
 #include "main.h"
+
 /**
- * wilcmp - compare two string
- * @s1: string 1
- * @s2: string 2, it can contain a
- * as a special character
- * Return: 1 if are indentical, 0 if not
+ * substring_match - check if a substring after wildcards match s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not;
+ */
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == '\0' && *s2 == '*')
+return (substring_match(s1, s2 + 1, s2 + 1));
+if (*s1 == '\0' && *s2 != '\0')
+return (0);
+if (*s2 == '*')
+return (substring_match(s1, s2 + 1, s2 + 1));
+if (*s1 <= *s2)
+return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+else
+return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+}
+
+/**
+ * wildcmp - compares if string with wildcard matches
+ * @s1: one string
+ * @s2: one string
+ * Return: 1 if matched, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 = '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-return (0);
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '*')
-return (wildcmp(s1, s2 + 1) || wilcmp(s1 + 1, s2));
+else if (*s2 == '*')
+return (substring_match(s1, (s2 + 1), (s2 + 1)));
+else
 return (0);
 }

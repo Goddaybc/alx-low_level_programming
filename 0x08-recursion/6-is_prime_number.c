5972384f6efea2177cebc91e568625d63be49730
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-20 08:35:43 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,7 +2,7 @@
 int is_divisible(int num, int div);
 int is_prime_number(int n);
 /**
- * _is_divisible - check if a number is divisible
+ * is_divisible - check if a number is divisible
  * @num: The number to be checked
  * @div: The divisor
  * Return: if the number is divisible - 0

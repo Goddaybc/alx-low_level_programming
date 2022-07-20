b0d7a8c473cad41c308c57d7f27e782daccdf199
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-20 11:15:56 -0700

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ if (*s1 == '\0' && *s2 != '\0')
 return (0);
 if (*s2 == '*')
 return (substring_match(s1, s2 + 1, s2 + 1));
-if (*s1 <= *s2)
+if (*s1 == *s2)
 return (substring_match(s1 + 1, s2 + 1, after_wldcd));
 else
 return (substring_match(s1 + 1, after_wldcd, after_wldcd));

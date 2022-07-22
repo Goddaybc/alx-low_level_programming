16ea4341dc855d9ddc58d50978202dd85cf860d6
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-22 08:45:52 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - prints number of argument
+ * main - Prints number of arguement
  * @argc: argument count
- * @argv: arrays of pointer to CLI argument
+ * @argv: arrays of pointer to CLI arguement
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-*argv = *argv;
+*argc = *argv;
 printf("%s\n", argc - 1);
 return (0);
 }

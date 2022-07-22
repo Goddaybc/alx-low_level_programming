0684ed31debcab4d9a595605fdc9c7124972b7be
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-22 10:34:38 -0700

    5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * isInteger - checks if s is an integer
- * @s: string to check
- * Return: 0 or 1
- */
-int isInteger(const char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-{
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
-}
-return (1);
-}
-/**
- * main - adds positive numbers
- * @argc: int
- * @argv: list
+ * main - Prints the minimum number of coins
+ * @argc: count
+ * @argv: array
  * Return: 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-int i = 0, coinUsed = 0, coin = 0;
-int coins[] = {25, 10, 5, 2, 1};
-if (argc != 2)
+int cents, ncoins = 0;
+if (argc == 1 || argc > 2)
 {
 printf("Error\n");
 return (1);
 }
-if (isInteger(argv[1]))
+cents = atoi(argv[1]);
+while (cents > 0)
 {
-i = atoi(argv[i]);
-while (i > 0 && coin <= 4)
-{
-if (i >= coins[coin])
-{
-i -= coins[coin];
-coinUsed++;
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+ncoins += 1;
 }
-else
-{
-coin++;
-}
-}
-}
-printf("%i\n", coinUsed);
+printf("%d\n", ncoins);
 return (0);
 }

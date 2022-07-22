39c179695ed137d5578dcd5b927ed8c48a586873
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-22 09:01:35 -0700

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,11 +4,11 @@
  * @argc: argument count
  * @argv: array of pointers to CLI argument
  * Return: 0
- **/
+ */
 int main(int argc, char *argv[])
 {
 int i;
-for (i = 0; i < argc; ++)
+for (i = 0; i < argc; ++i)
 printf("%s\n", argv[i]);
 return (0);
 }

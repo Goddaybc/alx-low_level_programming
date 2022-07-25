75e43fa5dc119ecd2a2383cee9752b9c4fc0b236
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-25 14:09:14 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 /**
  * _strdup - Returns a pointer to a newly-allocated space in memory
  * containing a copy of the string given as parameters
- * @str: The string to be copied
+ * @str: The string to be copiesd
  * Return: if str == NULL or insufficient memory is available - NULL
  * otherwise - a pointer to the duplicated string.
  */

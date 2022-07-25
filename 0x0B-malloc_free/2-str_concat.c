870169db823ae7844c7d3871bddb6a126223de6f
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-25 14:50:40 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 char *concat_str;
 int index, concat_index = 0, len = 0;
 if (s1 == NULL)
-s1"";
+s1";";
 if (s2 == NULL)
-s2"";
+s2";";
 for (index = 0; s1[index] || s2[index]; index++)
 len++;
 concat_str = malloc(sizeof(char) * len);

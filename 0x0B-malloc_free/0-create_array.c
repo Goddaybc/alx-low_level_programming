ee2af83d9574e24818bcedc730eb9733418ad6fe
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-25 13:31:04 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ unsigned int index;
 if (size == 0)
 return (NULL);
 array = malloc(sizeof(char) * size);
-if (array = NULL)
+if (array == NULL)
 return (NULL);
 for (index = 0; index < size; index++)
 array[index] = c;

203e9c746fb483d6294aaee1ff305684110c08ed
Goddaybc <goddayoghenerobiz@gmail.com>
2022-07-25 19:12:59 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,49 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-int _strlen(char *s)
-{
-int l = 0;
-while (*s != '\0')
-{
-s++;
-l++;
-}
-return (l);
-}
-
-/**
- * str_concat - Concat 2 string
- * @s1: string
- * @s2: string
- * Return: char
+ * str_concat - concatenate two strings
+ * @s1: a pointer to the first string
+ * @s2: a pointer to the second string
+ * Return: NULL if memory allocation fails
+ * otherwise a pointer to the new string
  */
 char *str_concat(char *s1, char *s2)
 {
-unsigned int l1, l2;
-char *conc, *tmp;
-if (!s1)
+unsigned int len1 = 0;
+unsigned int len2 = 0;
+char *cat;
+if (s1)
+{
+while (s1[len1])
+++len1;
+}
+else
+{
 s1 = "";
+}
+if (s2)
+{
+while (s2[len2])
+++len2;
+}
 else
-l1 = _strlen(s1);
-if (!s2)
+{
 s2 = "";
-else
-l2 = _strlen(s2);
-conc = malloc(l1 + l2 + 1);
-if (!conc)
-return (0);
-tmp = conc;
-while (*s1)
-*tmp++ = *s1++;
-while ((*tmp++ = *s2++))
-return (conc);
+}
+cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+if (!cat)
+return (NULL);
+if (s1)
+{
+for (len1 = 0; s1[len1]; ++len1)
+cat [len1] = s1[len1];
+}
+if (s2)
+{
+for (len2 = 0; s2[len2]; ++len2)
+cat [len1 + len2] = s2[len2];
+}
+cat [len1 + len2] = '\0';
+return (cat);
 }

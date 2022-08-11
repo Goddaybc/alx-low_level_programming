7e12e03480f270d26f2a23477691e32f8073298b
Goddaybc <goddayoghenerobiz@gmail.com>
2022-08-11 14:08:20 -0700

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,15 +3,17 @@
 #include "lists.h"
 
 /**
- * free_list - Release the memory allocated for list
- * @head: A pointer to the first node of the list for free
+ * free_list - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
  */
-free_list(list_t *head)
+void free_list(list_t *head)
 {
-if (head)
-{
-free_list(head->next);
-if (head->str)
-free(head->str);
-free(head);
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
 }

daebee4823bac60d0d5754ad7a34a877cd9c6576
Goddaybc <goddayoghenerobiz@gmail.com>
2022-08-22 10:18:25 -0700

    fileio

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);

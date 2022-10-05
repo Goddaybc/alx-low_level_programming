2ddf9a56e074103e2f4ca0c95b41f4204ea68b7c
Goddaybc <goddayoghenerobiz@gmail.com>
2022-10-05 08:34:20 -0700

    dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o libfilename.so *.o
+gcc -c -fpic *.c
+gcc -shared -o libdynamic.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+rm *.o
